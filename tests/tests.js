cc537bf8bd839c7f51b2e53c4887d5822180d899
Kelly Dwan <kelly.dwan@gmail.com>
2016-05-15 19:22:02 -0400

    Remove basic test example

--- tests/tests.js
+++ tests/tests.js
@@ -1,8 +1,4 @@
 /*global QUnit */
-QUnit.test( 'a basic test example', function( assert ) {
-	var value = 'hello';
-	assert.equal( value, 'hello', 'We expect value to be hello' );
-});
 
 QUnit.module( 'wp.updates' );
 QUnit.test( 'Update lock', function( assert ) {
