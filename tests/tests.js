30171aa3c17e6facea893c83e55eca475f64eb02
Kelly Dwan <kelly.dwan@gmail.com>
2016-05-16 15:36:35 -0400

    Add decrementCount test - Set up a fake counter on the HTML page - Check initial values - Check values after decrementing a plugin update

--- tests/tests.js
+++ tests/tests.js
@@ -1,65 +1,85 @@
 /*global QUnit */
+jQuery( function( $ ) {
 
-QUnit.module( 'wp.updates' );
+	QUnit.module( 'wp.updates' );
 
-QUnit.test( 'Initially, the update lock should be false', function( assert ) {
-	assert.strictEqual( wp.updates.updateLock, false );
-});
+	QUnit.test( 'Initially, the update lock should be false', function( assert ) {
+		assert.strictEqual( wp.updates.updateLock, false );
+	});
 
-QUnit.test( 'The nonce should be set correctly', function( assert ) {
-	assert.equal( wp.updates.ajaxNonce, window._wpUpdatesSettings.ajax_nonce );
-});
+	QUnit.test( 'The nonce should be set correctly', function( assert ) {
+		assert.equal( wp.updates.ajaxNonce, window._wpUpdatesSettings.ajax_nonce );
+	});
 
-// QUnit.test( 'decrementCount correctly decreases the update number', function( assert ) {});
+	QUnit.test( 'decrementCount correctly decreases the update number', function( assert ) {
+		// Check initial values
+		var menuItemCount  = $( '#menu-plugins' ).find( '.plugin-count' ).eq( 0 ).text();
+		var screenReaderItemCount = $( '#wp-admin-bar-updates' ).find( '.screen-reader-text' ).text();
+		var adminItemCount = $( '#wp-admin-bar-updates' ).find( '.ab-label' ).text();
+		assert.equal( menuItemCount, 2, 'Intial value is correct' );
+		assert.equal( screenReaderItemCount, '2 Plugin Updates', 'Intial value is correct' );
+		assert.equal( adminItemCount, 2, 'Intial value is correct' );
 
-QUnit.test( '`beforeunload` should only fire when locked', function( assert ) {
-	wp.updates.updateLock = false;
-	assert.notOk( wp.updates.beforeunload(), '`beforeunload` should not fire.' );
-	wp.updates.updateLock = true;
-	assert.equal( wp.updates.beforeunload(), window._wpUpdatesSettings.l10n.beforeunload, '`beforeunload` should equal the localized `beforeunload` string.' );
-	wp.updates.updateLock = false;
-});
+		wp.updates.decrementCount( 'plugin' );
 
-// FTP creds... exist?
-// Admin notice?
+		// Re-read these values
+		menuItemCount  = $( '#menu-plugins' ).find( '.plugin-count' ).eq( 0 ).text();
+		screenReaderItemCount = $( '#wp-admin-bar-updates' ).find( '.screen-reader-text' ).text();
+		adminItemCount = $( '#wp-admin-bar-updates' ).find( '.ab-label' ).text();
+		assert.equal( menuItemCount, 1 );
+		assert.equal( screenReaderItemCount, '1 Plugin Update' );
+		assert.equal( adminItemCount, 1 );
+	});
 
-QUnit.module( 'wp.updates.plugins' );
+	QUnit.test( '`beforeunload` should only fire when locked', function( assert ) {
+		wp.updates.updateLock = false;
+		assert.notOk( wp.updates.beforeunload(), '`beforeunload` should not fire.' );
+		wp.updates.updateLock = true;
+		assert.equal( wp.updates.beforeunload(), window._wpUpdatesSettings.l10n.beforeunload, '`beforeunload` should equal the localized `beforeunload` string.' );
+		wp.updates.updateLock = false;
+	});
 
-// QUnit.test( 'Update lock is set when plugins are updating', function( assert ) {});
+	// FTP creds... exist?
+	// Admin notice?
 
-QUnit.test( 'Plugins are queued when the lock is set', function( assert ) {
-	var value = [
-		{
-			type: 'update-plugin',
-			data: {
-				plugin: 'test/test.php',
-				slug: 'test'
+	QUnit.module( 'wp.updates.plugins' );
+
+	// QUnit.test( 'Update lock is set when plugins are updating', function( assert ) {});
+
+	QUnit.test( 'Plugins are queued when the lock is set', function( assert ) {
+		var value = [
+			{
+				type: 'update-plugin',
+				data: {
+					plugin: 'test/test.php',
+					slug: 'test'
+				}
 			}
-		}
-	];
+		];
 
-	window.pagenow = 'plugins';
-	wp.updates.updateLock = true;
-	wp.updates.updatePlugin( 'test/test.php', 'test' );
+		window.pagenow = 'plugins';
+		wp.updates.updateLock = true;
+		wp.updates.updatePlugin( 'test/test.php', 'test' );
 
-	assert.deepEqual( wp.updates.updateQueue, value );
+		assert.deepEqual( wp.updates.updateQueue, value );
 
-	delete window.pagenow;
-	wp.updates.updateLock = false;
-	wp.updates.updateQueue = [];
+		delete window.pagenow;
+		wp.updates.updateLock = false;
+		wp.updates.updateQueue = [];
+	});
+
+	// QUnit.test( 'If plugins are installing (lock is set), the beforeUnload function should fire', function( assert ) {} );
+	// QUnit.test( 'Starting a plugin update should call the update API (?)', function( assert ) {} );
+	// QUnit.test( 'Installing a plugin should call the API', function( assert ) {} );
+	// QUnit.test( 'Deleting a plugin should call the API', function( assert ) {} );
+	// QUnit.test( 'A successful update changes the message?', function( assert ) {} );
+	// QUnit.test( 'A failed update changes the message?', function( assert ) {} );
+
+	QUnit.module( 'wp.updates.themes' );
+	// QUnit.test( 'If themes are installing (lock is set), the beforeUnload function should fire', function( assert ) {} );
+	// QUnit.test( 'Starting a theme update should call the update API (?)', function( assert ) {} );
+	// QUnit.test( 'Installing a theme should call the API', function( assert ) {} );
+	// QUnit.test( 'Deleting a theme should call the API', function( assert ) {} );
+	// QUnit.test( 'A successful update changes the message?', function( assert ) {} );
+	// QUnit.test( 'A failed update changes the message?', function( assert ) {} );
 });
-
-// QUnit.test( 'If plugins are installing (lock is set), the beforeUnload function should fire', function( assert ) {} );
-// QUnit.test( 'Starting a plugin update should call the update API (?)', function( assert ) {} );
-// QUnit.test( 'Installing a plugin should call the API', function( assert ) {} );
-// QUnit.test( 'Deleting a plugin should call the API', function( assert ) {} );
-// QUnit.test( 'A successful update changes the message?', function( assert ) {} );
-// QUnit.test( 'A failed update changes the message?', function( assert ) {} );
-
-QUnit.module( 'wp.updates.themes' );
-// QUnit.test( 'If themes are installing (lock is set), the beforeUnload function should fire', function( assert ) {} );
-// QUnit.test( 'Starting a theme update should call the update API (?)', function( assert ) {} );
-// QUnit.test( 'Installing a theme should call the API', function( assert ) {} );
-// QUnit.test( 'Deleting a theme should call the API', function( assert ) {} );
-// QUnit.test( 'A successful update changes the message?', function( assert ) {} );
-// QUnit.test( 'A failed update changes the message?', function( assert ) {} );
